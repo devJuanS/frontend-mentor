8d4aba08b87088661b3ee5e76f3430baf074ad9a
Juan Sebastian Echeverry Parada <jsebast32@gmail.com>
2024-04-10 11:35:58 -0500

    define color styles and container to render the app.

--- time-tracking-dashboard/main.js
+++ time-tracking-dashboard/main.js
@@ -1,9 +1,14 @@
-import './style.css'
+import { TimeTrackApp } from './src/time-track-app';
+import './style.css';
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <div class="card">
+    <div class="dashboard">
 
     </div>
   </div>
-`
+`;
+
+const element = document.querySelector('.dashboard');
+
+TimeTrackApp(element);
