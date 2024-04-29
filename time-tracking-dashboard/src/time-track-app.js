af20d8746323f812acc90843cc38e0b028cdc54c
Juan Sebastian Echeverry Parada <jsebast32@gmail.com>
2024-04-29 11:08:47 -0500

    remove render-task-card module replaced by task-card component

--- time-tracking-dashboard/src/time-track-app.js
+++ time-tracking-dashboard/src/time-track-app.js
@@ -1,5 +1,4 @@
 import { renderNavbarDashboard } from "./presentation/render-navbar-dashboard/render-navbar-dashboard";
-import { renderTaskCard } from "./presentation/render-task-card/render-task-card";
 import { renderTasksBoard } from "./presentation/render-tasks-board/render-tasks-board";
 
 export const TimeTrackApp = ( element ) => {
