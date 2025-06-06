3ffd065db6bc7e0c8bc1b25ed15b094383ab4c18
Juan Sebastian Echeverry Parada <jsebast32@gmail.com>
2025-06-06 09:15:08 -0500

    initial commit.

---
+++ mortgage-repayment-calc/src/main.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+  </div>
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+    Coded by <a href="https://github.com/devJuanS">devJuanS</a>.
+  </div>
+`;
