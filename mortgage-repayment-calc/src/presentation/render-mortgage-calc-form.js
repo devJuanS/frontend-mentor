7172cd28037c5a723c5a3eb7928883597bf5691c
Juan Sebastian Echeverry Parada <jsebast32@gmail.com>
2025-06-19 10:56:00 -0500

    build(form): add presentation and some states styles to the calc form for inputs and button

--- mortgage-repayment-calc/src/presentation/render-mortgage-calc-form.js
+++ mortgage-repayment-calc/src/presentation/render-mortgage-calc-form.js
@@ -1,42 +1,49 @@
 import { validateCalcForm } from '../lib/helpers/validate-calc-form';
 import { calculateRepayment } from '../lib/helpers/calculate-repayments';
 import { renderMortgageCalcResults } from './render-mortgage-calc-results';
-import './render-mortgage-calc-form.css';
 import { cleanValidationErrors } from './clean-validation-errors';
 import { REQUIRED_CLASS_STATUS } from '../lib/constants/constants';
+import './render-mortgage-calc-form.css';
 
 const formHTML = `
-  <form id="mortgageCalcForm" novalidate>
+  <form id="mortgageCalcForm" novalidate class="stack--space-xl">
     <div class="calc-form__header">
       <h2 class="calc-form__title">Mortgage Calculator</h2>
-      <input type="reset" value="Clear All" />
+      <input type="reset" class="calc-form__reset" value="Clear All" />
     </div>
-    <div class="calc-form__inputs stack-s">
-      <div class="user-input stack-s">
+    <div class="calc-form__inputs stack--space-xl">
+      <div class="user-input stack--space-s">
         <label for="mortgageAmount">Mortgage Amount</label>
         <input id="mortgageAmount" type="number" name="mortgageAmount" class="input--numeric" min="0" required />
       </div>
-      <div class="inputs__mortgage-conditions stack-s">
-        <div class="user-input stack-s">
+      <div class="inputs__mortgage-conditions stack--space-xl">
+        <div class="user-input stack--space-s">
           <label for="mortgageTerm">Mortgage Term</label>
           <input id="mortgageTerm" type="number" name="mortgageTerm" class="input--numeric" min="0" required />
         </div>
-        <div class="user-input stack-s">
+        <div class="user-input stack--space-s">
           <label for="interestRate">Interest Rate</label>
           <input id="interestRate" type="number" name="interestRate" class="input--numeric" min="0" required />
         </div>
       </div>
     </div>
-    <div class="user-input stack-s">
-      <fieldset class="calc-form__mortgage-types user-input">
+    <div class="user-input">
+      <fieldset class="calc-form__mortgage-types user-input stack--space-s">
         <legend>Mortgage Type</legend>
-        <input id="mortgageTypeRepayment" type="radio" name="mortgageTypes" value="repayment" />
-        <label for="mortgageTypeRepayment" title="Repay capital and interest together.">Repayment</label>
-        <input id="mortgageTypeInterest" type="radio" name="mortgageTypes" value="interest" />
-        <label for="mortgageTypeInterest" title="Repay the interest. Capital paid off at end of term.">Interest Only</label>
+        <div class="mortgage-type-option">
+          <input id="mortgageTypeRepayment" type="radio" name="mortgageTypes" value="repayment" />
+          <label for="mortgageTypeRepayment" title="Repay capital and interest together.">Repayment</label>
+        </div>
+        <div class="mortgage-type-option">
+          <input id="mortgageTypeInterest" type="radio" name="mortgageTypes" value="interest" />
+          <label for="mortgageTypeInterest" title="Repay the interest. Capital paid off at end of term.">Interest Only</label>
+        </div>
       </fieldset>
     </div>
-    <input type="submit" value="Calculate Repayments" />
+    <button type="submit" class="button-submit">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path fill="#133041" d="M18.75 2.25H5.25a1.5 1.5 0 0 0-1.5 1.5v16.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5V3.75a1.5 1.5 0 0 0-1.5-1.5Zm-10.5 16.5a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm0-3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25ZM12 18.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25ZM12 15a1.125 1.125 0 1 1 0-2.25A1.125 1.125 0 0 1 12 15Zm3.75 3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm0-3.75a1.125 1.125 0 1 1 0-2.25 1.125 1.125 0 0 1 0 2.25Zm1.5-5.25a.75.75 0 0 1-.75.75h-9a.75.75 0 0 1-.75-.75V6a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 .75.75v3.75Z"/></svg>
+      <span>Calculate Repayments</span>
+    </button>
   </form>
 `;
 
